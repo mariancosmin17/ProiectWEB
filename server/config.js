14d55c4626a041a05375496dc98d88f5ce928e06
mariancosmin17 <mariancosmin58@yahoo.com>
2025-05-27 18:36:44 +0300

    Modificare config.js pt a adauga suport DocBook

--- server/config.js
+++ server/config.js
@@ -2,5 +2,6 @@ module.exports = {
   PORT: 8080,
   SALT_ROUNDS: 10,
   SECRET_KEY: 'cevaFoarteSecret123',
-  DB_PATH: './database.db'
+  DB_PATH: './database.db',
+  DOCBOOK_PATH: './data/abrevieri.xml'
 };
